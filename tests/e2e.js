a7ac2d167c866cfc2d45446fd45c93f9c9310e01
jannakha <j_malikova@yahoo.com>
2016-03-03 15:33:22 +1000

    added nightwatch tests

--- tests/e2e.js
+++ tests/e2e.js
@@ -4,8 +4,8 @@ module.exports = {
         .url('http://assets.library.uq.edu.au/master/pages/index.html')
         //.url('http://localhost:5000')
         .waitForElementVisible('body', 1000)
-        .assert.elementPresent('uq-minimal-header', 'uq header is missing')
-        .assert.elementPresent('uql-menu', 'uq menu is dis')
+        .assert.elementPresent('uq-minimal-header', 'uq header is displayed')
+        .assert.elementPresent('uql-menu', 'uq menu is displayed')
         .assert.elementPresent('uql-connect-footer', 'uq connect footer is displayed')
         .assert.elementPresent('uq-minimal-footer', 'uq footer is displayed')
         .end();
