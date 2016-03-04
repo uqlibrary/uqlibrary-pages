99a452e069b67e4004d0899618347d7d290552f2
jannakha <j_malikova@yahoo.com>
2016-03-04 10:26:04 +1000

    added local test

---
+++ bin/nightwatch-local.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var Logger = require('../node_modules/nightwatch/lib/util/logger.js');
+var Nightwatch = require('../node_modules/nightwatch/lib/index.js');
+
+try {
+
+  Nightwatch.cli(function(argv) {
+    console.log(Nightwatch);
+    Nightwatch.runner(argv);
+  });
+
+} catch (ex) {
+  Logger.error('There was an error while starting the test runner:\n\n');
+  process.stderr.write(ex.stack + '\n');
+  process.exit(2);
+}
+
