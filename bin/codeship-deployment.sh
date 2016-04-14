ab48e20325a06d1f0204ce0b51cef965a29b54ff
jannakha <j_malikova@yahoo.com>
2016-04-14 16:51:54 +1000

    fixed relative/absolut path for manifest

--- bin/codeship-deployment.sh
+++ bin/codeship-deployment.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 
-# start debugging/tracing commands, -e - exit if command returns error (non-zero status)
-set -e
-
-echo "Install prerequisites gulp/bower/packages"
-
-npm install -g gulp bower
-
-npm install
-bower install
-
-echo "Deploying branch: ${CI_BRANCH}"
-
-branch=${CI_BRANCH}
-src=$(git rev-parse --show-toplevel)
-base=$(basename ${src})
-dest="${base/uqlibrary-/}"
-
-pwd
-cd ../${base}
-pwd
-
-#echo "Check file syntax"
-##gulp syntax
-
-echo "Build distribution"
-gulp
-
-#echo "Update app cache manifest version"
-#appcache="dist/index.appcache"
-#version=$(git rev-parse HEAD)
+## start debugging/tracing commands, -e - exit if command returns error (non-zero status)
+#set -e
 #
-#sed -i -e "s#<VERSION>#${version}#g" ${appcache}
-
-# use codeship branch environment variable to push to branch name dir unless it's 'production' branch (or master for now)
-if [ ${CI_BRANCH} != "production" ]; then
-  export S3BucketSubDir=/${CI_BRANCH}/${dest}
-  export InvalidationPath=/${CI_BRANCH}/${dest}
-else
-  export S3BucketSubDir=${dest}
-  export InvalidationPath=/${dest}
-fi
-
-echo "Deploying to S3 bucket sub-dir: ${S3BucketSubDir}"
-echo "Prepare AWS configuration..."
-
-# Use env vars to set AWS config
-awsconfigtemp="template.aws.json"
-awsconfig="aws.json"
-
-cp $awsconfigtemp $awsconfig
-
-sed -i -e "s#<AWSAccessKeyId>#${AWSAccessKeyId}#g" ${awsconfig}
-sed -i -e "s#<AWSSecretKey>#${AWSSecretKey}#g" ${awsconfig}
-sed -i -e "s#<S3Bucket>#${S3Bucket}#g" ${awsconfig}
-sed -i -e "s#<S3BucketSubDir>#${S3BucketSubDir}#g" ${awsconfig}
-sed -i -e "s#<CFDistribution>#${CFDistribution}#g" ${awsconfig}
-sed -i -e "s#<AWSRegion>#${AWSRegion}#g" ${awsconfig}
+#echo "Install prerequisites gulp/bower/packages"
+#
+#npm install -g gulp bower
+#
+#npm install
+#bower install
+#
+#echo "Deploying branch: ${CI_BRANCH}"
+#
+#branch=${CI_BRANCH}
+#src=$(git rev-parse --show-toplevel)
+#base=$(basename ${src})
+#dest="${base/uqlibrary-/}"
+#
+#pwd
+#cd ../${base}
+#pwd
+#
+##echo "Check file syntax"
+###gulp syntax
+#
+#echo "Build distribution"
+#gulp
+#
+##echo "Update app cache manifest version"
+##appcache="dist/index.appcache"
+##version=$(git rev-parse HEAD)
+##
+##sed -i -e "s#<VERSION>#${version}#g" ${appcache}
+#
+## use codeship branch environment variable to push to branch name dir unless it's 'production' branch (or master for now)
+#if [ ${CI_BRANCH} != "production" ]; then
+#  export S3BucketSubDir=/${CI_BRANCH}/${dest}
+#  export InvalidationPath=/${CI_BRANCH}/${dest}
+#else
+#  export S3BucketSubDir=${dest}
+#  export InvalidationPath=/${dest}
+#fi
+#
+#echo "Deploying to S3 bucket sub-dir: ${S3BucketSubDir}"
+#echo "Prepare AWS configuration..."
+#
+## Use env vars to set AWS config
+#awsconfigtemp="template.aws.json"
+#awsconfig="aws.json"
+#
+#cp $awsconfigtemp $awsconfig
+#
+#sed -i -e "s#<AWSAccessKeyId>#${AWSAccessKeyId}#g" ${awsconfig}
+#sed -i -e "s#<AWSSecretKey>#${AWSSecretKey}#g" ${awsconfig}
+#sed -i -e "s#<S3Bucket>#${S3Bucket}#g" ${awsconfig}
+#sed -i -e "s#<S3BucketSubDir>#${S3BucketSubDir}#g" ${awsconfig}
+#sed -i -e "s#<CFDistribution>#${CFDistribution}#g" ${awsconfig}
+#sed -i -e "s#<AWSRegion>#${AWSRegion}#g" ${awsconfig}
 
 echo "Set all files to absolut paths..."
 distFolder=$(ls -d dist/*.html)
@@ -75,7 +75,7 @@ toReplace=(
   "src=\"scripts/"
   "content=\"images/"
   "src=\"images/"
-  "manifest=\"/pages"
+  "manifest=\"/pages/"
   )
 replaceWith=(
   "href=\"${absolutePath}/manifest.json\""
@@ -86,7 +86,7 @@ replaceWith=(
   "src=\"${absolutePath}/scripts/"
   "content=\"${absolutePath}/images/"
   "src=\"${absolutePath}/images/"
-  "manifest=\"${absolutePath}/pages"
+  "manifest=\"${absolutePath}/"
   )
 
 for htmlFile in ${distFolder[@]}; do
@@ -96,13 +96,13 @@ for htmlFile in ${distFolder[@]}; do
     index=${index}+1
   done
 done
-
-echo "Run gulp task to upload to AWS..."
-gulp publish
-
-echo "Run Cloudfront Invalidation: " gulp invalidate --path ${InvalidationPath}
-gulp invalidate --path ${InvalidationPath}
-
-echo "Clean up AWS configuration..."
-rm -f ${awsconfig}
+#
+#echo "Run gulp task to upload to AWS..."
+#gulp publish
+#
+#echo "Run Cloudfront Invalidation: " gulp invalidate --path ${InvalidationPath}
+#gulp invalidate --path ${InvalidationPath}
+#
+#echo "Clean up AWS configuration..."
+#rm -f ${awsconfig}
 
