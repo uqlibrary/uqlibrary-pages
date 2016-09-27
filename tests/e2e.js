400e7411d100019a2ffc89aa5adfaca6bdda2846
Lea de Groot <l.degroot@library.uq.edu.au>
2016-09-27 15:10:45 +1000

    try testing on outer element[PT #119335435]

--- tests/e2e.js
+++ tests/e2e.js
@@ -3,7 +3,7 @@ module.exports = {
     client
         .url('http://dev-app.library.uq.edu.au:5001')
         .resizeWindow(1280, 800)
-        .waitForElementVisible('uql-global-links', 10000)
+        .waitForElementVisible('uql-menu', 10000)
         .assert.elementPresent('uq-minimal-header', 'uq header component is present')
         .assert.elementPresent('uq-minimal-header uql-global-links', 'uq global links component is present')
         .assert.elementPresent('uql-menu', 'uq menu component is present')
@@ -11,6 +11,7 @@ module.exports = {
         .assert.elementPresent('uql-connect-footer', 'uq connect footer component is present')
         .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
         .assert.elementPresent('paper-tabs', 'paper-tabs component is present')
+        .saveScreenshot('screenshots/desktop.png')
         .end();
   }
 };
--- tests/e2e.mobile.js
+++ tests/e2e.mobile.js
@@ -4,13 +4,14 @@ module.exports = {
       .url('http://dev-app.library.uq.edu.au:5001')
       .resizeWindow(600, 800)
       .waitForElementVisible('uql-menu-button', 10000)
-      .assert.elementPresent('uql-menu-button', 'in mobile view')
+      .assert.elementPresent('uql-menu-button', 'uql-menu-button present in mobile view')
       .assert.elementPresent('uq-minimal-header', 'uq header component is present')
-      .assert.hidden('uq-minimal-header uql-global-links', 'uq global links component is hidden')
+      .assert.hidden('uq-minimal-header uql-global-links', 'header uq global links component is hidden in mobile view')
       .assert.elementPresent('uql-menu', 'uq menu component is present')
       .assert.elementPresent('uql-connect-footer', 'uq connect footer component is present')
       .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
       .assert.elementPresent('paper-tabs', 'paper-tabs component is present')
+      .saveScreenshot('screenshots/mobile.png')
       .end();
   }
 };
