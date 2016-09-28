c91a34faa07a2c2b5f5318ef94ddf0f50ce981cc
Lea de Groot <l.degroot@library.uq.edu.au>
2016-09-28 11:45:35 +1000

    title text test

--- tests/e2e.js
+++ tests/e2e.js
@@ -10,7 +10,10 @@ module.exports = {
         .assert.hidden('uql-menu-button', 'uq hamburger menu button uql-menu-button component is hidden')
         .assert.elementPresent('uql-connect-footer', 'uq connect footer component is present')
         .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
+
         .assert.elementPresent('paper-tabs', 'paper-tabs component is present')
+        .assert.elementPresent('.title-text', 'Search Title is present')
+        .assert.containsText('.title-text', 'UQ Library Search')
 .saveScreenshot('screenshots/e2ejs.png')
         .end();
   }
