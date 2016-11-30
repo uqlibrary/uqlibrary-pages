c320cd49b0f772701f7a298e439a8cc3e2133c5e
jannakha <j_malikova@yahoo.com>
2016-11-30 14:29:36 +1000

    updated codeship scripts

--- bin/codeship-setup.sh
+++ bin/codeship-setup.sh
@@ -6,14 +6,15 @@ set -e
 echo "Install prerequisites gulp/bower/packages"
 
 echo "Check Java version is set to Java8"
+# can't run jdk_switcher in script
+# jdk_switcher use oraclejdk8
+
 version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
 echo version "$version"
 if [[ "$version" < "1.8" ]]; then
   echo "Java version is too old, min Java8 is required"
+  return 1
 fi
-
-# can't run jdk_switcher in script
-# jdk_switcher use oraclejdk8
 
 echo "Install dependencies"
 npm install -g gulp bower
