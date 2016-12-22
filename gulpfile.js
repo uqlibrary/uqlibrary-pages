d8ab44280e01c9d91cfc1b0de4abb18cb4031da2
Ky <k.lane@uq.edu.au>
2016-12-22 11:07:08 +1000

    Updating dependencies (gulp-useref v3 api migration)

--- gulpfile.js
+++ gulpfile.js
@@ -66,21 +66,26 @@ var imageOptimizeTask = function(src, dest) {
 };
 
 var optimizeHtmlTask = function(src, dest) {
-  var assets = $.useref.assets({
-    searchPath: ['.tmp', 'app']
-  });
+  var assets = $.useref.assets();
+  // {
+  //  searchPath: ['.tmp', 'app']
+  // });
 
   return gulp.src(src)
     .pipe(assets)
+
     // Concatenate and minify JavaScript
     .pipe($.if('*.js', $.uglify({
       preserveComments: 'some'
     })))
+
     // Concatenate and minify styles
     // In case you are still using useref build blocks
     .pipe($.if('*.css', $.minifyCss()))
     .pipe(assets.restore())
     .pipe($.useref())
+
+
     // Minify any HTML
     .pipe($.if('*.html', $.minifyHtml({
       quotes: true,
