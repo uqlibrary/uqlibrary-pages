d6cd97f01d9f72fd8c0f4e20b6cf5addd4d7731b
Lea de Groot <l.degroot@library.uq.edu.au>
2018-10-29 10:59:05 +1000

    update the tests for removed elements [PT #160976682]

--- tests/e2e.minimal.js
+++ tests/e2e.minimal.js
@@ -10,13 +10,11 @@ module.exports = {
             .assert.elementPresent('uql-menu', 'uq menu component is present')
             .assert.elementPresent('uql-connect-footer', 'uq connect footer component is present')
             .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
-            .assert.containsText('.library-title a', 'UQ Library')
+            .assert.containsText('.library-title a', 'Library')
             .assert.elementPresent('uql-askus-button', 'uql askus component is present')
             .assert.elementPresent('uql-search-button', 'uql search button is present')
             .assert.elementPresent('uql-login-button', 'uql login button is present')
             .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
-            .assert.elementPresent('uq-minimal-footer uql-global-links', 'uq global links footer component is present')
-            .assert.containsText('uq-minimal-footer .footer-uq-details li', 'Authorised by:')
             .assert.elementPresent('uq-minimal-footer .footer-legal-details a', 'Emergency Phone footer component is present')
             .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency');
 
