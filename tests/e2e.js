07cf0462ab146294d47ffd019f1f8f3765e80dcd
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-01-07 12:42:06 +1000

    Run IE-specific test IE only

--- tests/e2e.js
+++ tests/e2e.js
@@ -18,11 +18,11 @@ module.exports = {
             // if IE11 fails because ES2016 syntax is used it will cause the iron-icons to be huuuge
             // this test will (hopefully) catch this so we can fix it before prod - or decide not to support IE11
             .assert.elementPresent('uqlibrary-search paper-button iron-icon', 'catalog search button is present')
-            .getElementSize('uqlibrary-search paper-button iron-icon', function(element) {
-                // safari is returning 25 not 24 here, but appears fine on saucelabs - we are really checking that the icons
-                // have drawn properly, and arent like 100px tall, so comparing against a reasonable size is fine
-                this.assert.ok(element.value.width < 26, 'check catalog search button is correct width');
-                this.assert.ok(element.value.height < 26, 'check catalog search button is correct height');
+            .getElementSize('uqlibrary-search paper-button iron-icon', function(element) {                
+                if (client.capabilities.browserName === 'internet explorer') {
+                    this.assert.equal(element.value.width, 24, 'check catalog search button is correct width');
+                    this.assert.equal(element.value.height, 24, 'check catalog search button is correct height');
+                }
             });
 
         minimalUql.commonChecks(client, urlTest, 1280, 1000);
