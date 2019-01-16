6d877169e1f594a047a73e680e4cb82a0d5e4301
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-01-16 15:29:33 +1000

    Fix scope of IE-specific test

--- tests/e2e.js
+++ tests/e2e.js
@@ -16,16 +16,17 @@ module.exports = {
             .assert.elementPresent('uqlibrary-search', 'search component is present')
             .assert.elementPresent('iron-pages', 'iron-pages component is present')
             .assert.hidden('uql-menu-button', 'uq hamburger menu button uql-menu-button component is hidden')
-
-            // if IE11 fails because ES2016 syntax is used it will cause the iron-icons to be huuuge
-            // this test will (hopefully) catch this so we can fix it before prod - or decide not to support IE11
             .assert.elementPresent('uqlibrary-search paper-button iron-icon', 'catalog search button is present')
-            .getElementSize('uqlibrary-search paper-button iron-icon', function(element) {                
-                if (client.capabilities.browserName === 'internet explorer') {
-                    this.assert.equal(element.value.width, 24, 'check catalog search button is correct width');
-                    this.assert.equal(element.value.height, 24, 'check catalog search button is correct height');
-                }
+        ;
+        
+        // if IE11 fails because ES2016 syntax is used it will cause the iron-icons to be huuuge
+        // this test will (hopefully) catch this so we can fix it before prod - or decide not to support IE11
+        if (client.capabilities.browserName === 'internet explorer') {
+            client.getElementSize('uqlibrary-search paper-button iron-icon', function(element) {
+                this.assert.equal(element.value.width, 24, 'check catalog search button is correct width');
+                this.assert.equal(element.value.height, 24, 'check catalog search button is correct height');
             });
+        }
 
         minimalUql.commonChecks(client, urlTest, clientWidth, clientHeight);
 
