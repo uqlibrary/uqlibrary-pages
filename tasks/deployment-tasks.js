936050923aa9851969644184bdbdfb7fec72d67f
Lea de Groot <l.degroot@library.uq.edu.au>
2019-08-06 12:23:21 +1000

    fix the invalidation path for feature branches [PT #167694043]

--- tasks/deployment-tasks.js
+++ tasks/deployment-tasks.js
@@ -11,7 +11,9 @@ var cloudfront = require('gulp-invalidate-cloudfront');
 
 var path = require('path');
 var fs = require('fs');
-var argv = require('yargs/yargs').argv;
+var argv = require('yargs/yargs')(process.argv.slice(2));
+
+$.util.log('parameter passed: ', argv);
 
 var DIST = 'dist';
 var dist = function(subpath) {
@@ -19,20 +21,20 @@ var dist = function(subpath) {
 };
 
 /**
- * Command line param:
- *    --path {INVALIDATION_PATH}
+ * usage:
+ *  gulp invalidate --path {INVALIDATION_PATH}
  *
- * If no bucket path passed will invalidate production subdir
+ * If no bucket folder-path passed, it will invalidate the production subdirectory
  */
 gulp.task('invalidate', function () {
   var awsConfig = JSON.parse(fs.readFileSync('./aws.json', 'utf8'));
 
   var invalidatePath = '';
 
-  if (argv.path === undefined) {
-    invalidatePath += '/pages/*';
-  } else {
+  if (!!argv.path) {
     invalidatePath = argv.path + '/*';
+  } else {
+    invalidatePath = '/pages/*';
   }
 
   $.util.log('Invalidation path: ' + invalidatePath);
