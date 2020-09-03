b33fe15dcbb02c7645c82f8a643d12f9e3a9cf53
Dan Callan <d.callan@library.uq.edu.au>
2020-09-03 16:48:25 +1000

    Added codebuild specific setup script

---
+++ bin/codebuild-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# start debugging/tracing commands, -e - exit if command returns error (non-zero status)
+set -e
+
+echo "Check Java version is 8"
+# can't run jdk_switcher in script
+# jdk_switcher use oraclejdk8
+version=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+echo "Java version is $version"
+if [[ "$version" != "8" ]]; then
+  echo "Java 8 is required"
+  exit 1
+fi
+
+# We are running as root in codebuild
+sed -i -e "s#bower update#bower --allow-root update#g" "package.json"
+
+printf "Node "; node -v;
+printf "(Before npm i -g) npm v"; npm -v
+
+printf "\n\n--- Install dependencies ---\n"
+npm install -g gulp-cli bower nightwatch npm@6
+npm install
+
+# node_modules from reusable repo required
+cd app/bower_components/uqlibrary-reusable-components/
+npm install
+cd ../../../
+
+npm rebuild node-sass
